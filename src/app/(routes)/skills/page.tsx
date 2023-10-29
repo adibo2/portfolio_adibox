e534e4118607eebb561b0e0f65c0f87ba80a9606
adibo2 <Adibbensmina99@gmail.com>
2023-10-29 23:59:28 +0100

    first commit

--- src/app/(routes)/skills/page.tsx
+++ src/app/(routes)/skills/page.tsx
@@ -5,7 +5,7 @@ import { Transition } from "@/components/Transition/Transition";
 
 export default function AboutPage() {
     return (
-        <div className=" flex flex-col  mt-4 justify-center md:mt-10 min-h-[108vh]">
+        <div className=" flex flex-col  mt-32 justify-center md:mt-10 min-h-[112vh]">
             <Transition />
             <Skills />
         </div>
