9f5e01d3b2aaca0490af8d59288eab8d410b93ae
adibo2 <Adibbensmina99@gmail.com>
2023-10-29 14:22:00 +0100

    first commit

--- src/components/Introduction/Introduction.tsx
+++ src/components/Introduction/Introduction.tsx
@@ -21,13 +21,13 @@ export function Introduction() {
                         animate="show"
                         exit="hidden"
                     >
-                        <h1 className="mb-3 text-center sm:text-start md:text-4xl text-base leading-tight md:mb-10">Hi 👋🏼.My name is,  <br />
+                        <h1 className="mb-3 text-center sm:text-start md:text-4xl text-lg  leading-tight md:mb-10">Hi 👋🏼.My name is,  <br />
                             <span className="text-secondary">ADIB BENSMINA</span></h1>
-                        <p className="max-w-xl  text-center sm:text-start  mb-10 leading-10 text-sm md:max-w-[37rem] md:mx-0 md:mb-16 md:text-[20px] md:leading-10 ">
+                        <p className="max-w-xl  text-center sm:text-start  mb-10 leading-5 text-base md:max-w-[37rem] md:mx-0 md:mb-16 md:text-[20px] md:leading-10 ">
                         I'm an aspiring software engineer equipped with skills in Java, JavaScript, React, Next.js, Docker and AWS cloud. As a recent graduate, I'm passionate about crafting digital products that put user needs first. My focus is on utilizing my expertise to collaborate with cross-functional teams, implement accessible and inclusive design, and rapidly build applications that provide seamless experiences across platforms. 
                         </p>
 
-                        <div className="flex flex-col items-center gap-3 md:gap-10 md:flex-row">
+                        <div className="flex  items-center gap-3 md:gap-10 md:flex-row">
                             <a href="/projects" className="px-6 py-3 my-2 transition-all border-2 cursor-pointer w-fit rounded-xl hover:shadow-xl hover:shadow-white/50">
                                 Resume
                             </a>
